e2f70ae07186528f843b17c4e8fe37056f984aba
angreno <singhkunaliitianbusiness1@gmail.com>
2024-01-11 09:17:54 +0530

    second commit

---
+++ src/j1_java_basics.java
@@ -0,0 +1,5 @@
+public class j1_java_basics {
+    public static void main(String[] args) {
+        System.out.println("jai mata di");
+    }
+}
---
+++ src/j2_printing_pattern.java
@@ -0,0 +1,13 @@
+public class j2_printing_pattern {
+    public static void main(String[] args){
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+        int a =5;
+        System.out.println(a);
+
+
+    }
+}
