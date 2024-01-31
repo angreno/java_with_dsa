af274048c8ac73784d4c256e5c04dc46ef336f2d
angreno <singhkunaliitianbusiness1@gmail.com>
2024-01-31 22:57:09 +0530

    add files

---
+++ src/j5_myth_about_files.java
@@ -0,0 +1,8 @@
+public class j5_myth_about_files {
+    public static  void main(String[]  args){
+        System.out.println("hello world");
+
+    }
+
+
+}
---
+++ src/jjj.java
@@ -0,0 +1,18 @@
+public class jjj {
+    public static void main(String[] args) {
+        String name1 = "Ganga";
+        String name2 = "Amazon";
+
+        boolean statement1 = name1 instanceof String;
+
+        /*boolean statement2 = name2 instanceof name1;*/
+
+        boolean statement3 = name1 instanceof Object;
+
+        boolean statement4 = args instanceof Object;
+
+        boolean statement5 = args instanceof String[];
+
+    }
+
+}
